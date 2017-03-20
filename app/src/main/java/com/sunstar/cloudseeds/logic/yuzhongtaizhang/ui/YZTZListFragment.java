639ea099cc058c46af5b61f6567e242851291e0c
classichu <classichu@qq.com>
2017-03-20 17:35:40 +0800

    update

--- app/src/main/java/com/sunstar/cloudseeds/logic/yuzhongtaizhang/ui/YZTZListFragment.java
+++ app/src/main/java/com/sunstar/cloudseeds/logic/yuzhongtaizhang/ui/YZTZListFragment.java
@@ -9,8 +9,10 @@ import android.widget.SearchView;
 
 import com.classichu.adapter.recyclerview.ClassicRVHeaderFooterAdapter;
 import com.classichu.adapter.widget.ClassicEmptyView;
+import com.classichu.classichu.app.CLog;
 import com.classichu.classichu.classic.ClassicMvpFragment;
 import com.classichu.dialogview.manager.DialogManager;
+import com.jakewharton.rxbinding2.widget.RxSearchView;
 import com.sunstar.cloudseeds.R;
 import com.sunstar.cloudseeds.data.AtyGoToWhere;
 import com.sunstar.cloudseeds.logic.scan.ScanQrCodeType;
@@ -26,6 +28,11 @@ import com.sunstar.cloudseeds.ui.SearchFragment;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -72,7 +79,7 @@ public class YZTZListFragment extends ClassicMvpFragment<YZTZListPresenterImpl> 
 
     @Override
     protected void initView(View view) {
-
+        initSearchView();
         //-- add by lzy -2017.3.20
         //隐藏搜索框
         from_searchFragment=false;
@@ -88,6 +95,59 @@ public class YZTZListFragment extends ClassicMvpFragment<YZTZListPresenterImpl> 
     @Override
     protected void initListener() {
 
+    }
+    private String mQueryText;
+    private void initSearchView() {
+        final SearchView searchView = findById(R.id.id_search_view);
+        //设置搜索图标是否显示在搜索框内
+        searchView.setIconifiedByDefault(false);//The default value is true   ，设置为false直接展开显示 左侧有放大镜  右侧无叉叉   有输入内容后有叉叉
+        //!!! searchView.setIconified(false);//true value will collapse the SearchView to an icon, while a false will expand it. 左侧无放大镜 右侧直接有叉叉
+        //  searchView.onActionViewExpanded();//直接展开显示 左侧无放大镜 右侧无叉叉 有输入内容后有叉叉 内部调用了setIconified(false);
+        //searchView.setImeOptions(EditorInfo.IME_ACTION_SEARCH);
+        searchView.setQueryHint("请输入关键字");//设置查询提示字符串
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                mQueryText = query;
+                toSearchData();
+                CLog.d("onQueryTextSubmit:" + query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(final String newText) {
+                //RxBinding
+                RxSearchView.queryTextChanges(searchView)
+                        .debounce(500, TimeUnit.MILLISECONDS)
+                        .observeOn(AndroidSchedulers.mainThread())
+                        //对用户输入的关键字进行过滤
+                        /*.filter(new Func1<CharSequence, Boolean>() {
+                            @Override
+                            public Boolean call(CharSequence charSequence) {
+                                return charSequence.toString().trim().length() > 0;
+                            }
+                        })*/
+                        .subscribe(new Consumer<CharSequence>() {
+                            @Override
+                            public void accept(@NonNull CharSequence charSequence) throws Exception {
+                                mQueryText = charSequence.toString();
+                                if (mQueryText.trim().length() > 0) {
+                                    toSearchData();
+                                } else {//空白
+                                    //刷新所有
+                                    toRefreshData();
+                                }
+                                CLog.d("queryTextChanges:" + mQueryText);
+                            }
+                        });
+
+                return false;
+            }
+        });
+    }
+
+    private void toSearchData() {
+        mPresenter.gainCountData(mClassicRVHeaderFooterAdapter.getNowPageCount(),mQueryText);
     }
 
     @Override
